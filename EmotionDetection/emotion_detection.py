2bc681b3f55461fda07b5d6199d78c953cb7b4eb
AhmedEFRMElwazery <91789961+AhmedEFRMElwazery@users.noreply.github.com>
2026-05-30 12:50:12 +0300

    added functionality to detect blank user entries and possible error handling scenarios

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,16 +14,46 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url=url, headers=headers, json=input_json, timeout=5)
     
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
 
-    dominant_emotion = ""
-    dominant_emotion_Score = 0
+        dominant_emotion = ""
+        dominant_emotion_Score = 0
 
-    for k, v in emotions.items():
-        if v > dominant_emotion_Score:
-            dominant_emotion_Score = v
-            dominant_emotion = k
+        for k, v in emotions.items():
+            if v > dominant_emotion_Score:
+                dominant_emotion_Score = v
+                dominant_emotion = k
 
-    return {**emotions, "dominant_emotion": dominant_emotion}
+        return {**emotions, "dominant_emotion": dominant_emotion}
+    elif response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion":None
+            }
+    elif response.status_code == 500:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion":None
+            }
+    else:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion":None
+            }
+        
+
 
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    if response["dominant_emotion"] == None:
+        return "Invalid text! Please try again!"    
+    formatted_response = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    
+    return formatted_response
+
+@app.route("/")
+def render_index_page():
+    
+    return render_template("index.html")
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
