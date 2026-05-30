8890231ba57c2c6d4f051c1d06c106e7f5f6dfb2
AhmedEFRMElwazery <91789961+AhmedEFRMElwazery@users.noreply.github.com>
2026-05-30 11:42:23 +0300

    created the Emotiondetection package and updated the README.md file

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,29 @@
+import json
+import requests
+"""
+This module uses IBM Watson NLP library's function for emotion detection
+"""
+
+def emotion_detector(text_to_analyze):
+    """
+    This function receives a text, and uses the Emotion Predict function of Watson NLP
+    """
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url=url, headers=headers, json=input_json, timeout=5)
+    
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = ""
+    dominant_emotion_Score = 0
+
+    for k, v in emotions.items():
+        if v > dominant_emotion_Score:
+            dominant_emotion_Score = v
+            dominant_emotion = k
+
+    return {**emotions, "dominant_emotion": dominant_emotion}
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Repository for final project
+
+## Final Project
+
+### Final Project: AI-Based Web Application Development and Deployment
