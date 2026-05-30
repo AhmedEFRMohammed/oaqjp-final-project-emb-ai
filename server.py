34bdd3b194dabae9e3b20efe6837f09b224b7423
AhmedEFRMElwazery <91789961+AhmedEFRMElwazery@users.noreply.github.com>
2026-05-30 13:06:18 +0300

    ran pylint and made adjustment until I got a score 10/10

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+This is a flask server for a web app frontend that takes
+texts from users, delivers it to IBM's Watson NLP library's 
+Emotion Detect function, and the returns tthe result to
+the front-end for the user to observe
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,19 +12,28 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    This is the handling function for the API endpoint that 
+    receives the request query to conduct emotion decetion on it.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
-    if response["dominant_emotion"] == None:
-        return "Invalid text! Please try again!"    
-    formatted_response = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-    
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+    formatted_response = f"For the given statement, the system response is \
+    'anger': {response['anger']}, 'disgust': {response['disgust']}, \
+    'fear': {response['fear']}, 'joy': {response['joy']} and \
+    'sadness': {response['sadness']}. The dominant emotion is \
+    {response['dominant_emotion']}."
     return formatted_response
 
 @app.route("/")
 def render_index_page():
-    
+    """
+    This is the handling function for the API endpoint that
+    delivers the landing page once the user visit the website
+    """
     return render_template("index.html")
-    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
